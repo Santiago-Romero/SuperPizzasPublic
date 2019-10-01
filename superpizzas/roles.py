5ea049fd929174bb6d226109a74480a5d5ca2713
DanielG <daga9420@gmail.com>
2019-09-30 23:09:44 -0500

    roles creados

---
+++ superpizzas/roles.py
@@ -0,0 +1,11 @@
+from rolepermissions.roles import AbstractUserRole
+
+class UsuarioFranquicia(AbstractUserRole):
+    available_permissions = {
+        
+    }
+
+class Cliente(AbstractUserRole):
+    available_permissions = {
+        
+    }
