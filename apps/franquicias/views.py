b1b6950776fb7171119dfa443878fb1a56f7afd9
Santiago-Romero <romeroclass97@gmail.com>
2019-10-04 21:41:20 -0500

    pequeños cambios para que quede claro la disposición de las urls

--- apps/franquicias/views.py
+++ apps/franquicias/views.py
@@ -12,7 +12,8 @@ def nada(request):
     return HttpResponse("<h2>Aqui iría la págia principal donde se venden franquicias, por ahora ir a <a href='http://localhost:8000/admin/'>http://localhost:8000/admin/</a> para gestionar franquicias</h2>")
 
 def nada_tenant(request):
-    return HttpResponse("<h2>Aqui iría la págia principal donde se venden las pizzas de cada tenant, por ahora ir a <a href='http://localhost:8000/admin/'>http://[nombre_tenant].localhost:8000/admin/</a> para gestionar la franquicia</h2>")
+    print(request.tenant)
+    return HttpResponse("<h2>Aqui iría la págia principal donde se venden las pizzas de cada tenant, por ahora ir a <a href='http://{d}.localhost:8000/admin/'>http://{d}.localhost:8000/admin/</a> para gestionar la franquicia</h2>".format(d=request.tenant.schema_name))
 
 
 def registrar_franquicia(request):
