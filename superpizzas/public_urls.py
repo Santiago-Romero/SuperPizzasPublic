3f8419056ff4a24f6df035547cd02794840fe8dd
Stephany Rivera <rivera.stephany@correounivalle.edu.co>
2019-10-07 16:57:24 -0500

    pasarela de pago front

--- superpizzas/public_urls.py
+++ superpizzas/public_urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import path,include
 from apps.franquicias.views import home, inicio_franquicia,compra_franquicia
 
+
 urlpatterns = [
     path('', inicio_franquicia, name='nada'),
-    path('compra/', compra_franquicia, name='nueva_franquicia'),
+    path('compra/', compra_franquicia, name='compra'),
     path('admin/', home, name='home'),
     path('admin/franquicias/', include('apps.franquicias.urls', namespace='franquicias')),
     #path('admin/', admin.site.urls),
