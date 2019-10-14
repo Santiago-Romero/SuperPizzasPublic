3f01d4b2649c6f97962e49bd469bd9f2aa51df2f
Santiago-Romero <romeroclass97@gmail.com>
2019-10-14 15:37:50 -0500

    Algunos ajustes a la compra de franquicia (tipos y mensaje de submit)

--- superpizzas/public_urls.py
+++ superpizzas/public_urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path,include
-from apps.franquicias.views import home, inicio_franquicia,compra_franquicia
+from apps.franquicias.views import *
 
 
 urlpatterns = [
