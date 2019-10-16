9488557976e8981aed9e2e73815337c4ba496e64
Stephany Rivera <rivera.stephany@correounivalle.edu.co>
2019-10-16 17:46:38 -0500

    login

--- apps/usuarios/forms.py
+++ apps/usuarios/forms.py
@@ -8,3 +8,7 @@ class UsuarioForm(forms.ModelForm):
         fields = '__all__'
         widgets = {'password': forms.PasswordInput(),
                     'rol': forms.HiddenInput(),}
+    
+class LoginForm(forms.Form):
+    nickname=forms.CharField(max_length=50,label="nickname")
+    password=forms.CharField(max_length=75,label="password",widget=forms.PasswordInput)
