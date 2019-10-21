8143d619af138f8a9dc067c67987d12fd32dcb38
Santiago-Romero <romeroclass97@gmail.com>
2019-10-21 18:28:47 -0500

    pequeño mensajes de login y render

--- superpizzas/tenant_urls.py
+++ superpizzas/tenant_urls.py
@@ -6,7 +6,7 @@ from apps.usuarios.views import login_view
 urlpatterns = [
     path('', nada_tenant, name='nada_tenant'),
     path('admin/', home, name='home'),
-    path('login/', login_view,name='login'),
+    path('login/', login_view,name='login'), 
     path('admin/pizzas/', include('apps.pizzas.urls', namespace='pizzas')),
     path('admin/usuarios/', include('apps.usuarios.urls', namespace='usuarios')),
     path('admin/ingredientes/', include('apps.ingredientes.urls', namespace='ingredientes')),
