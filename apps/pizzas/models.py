35fa3c481f6c931b3797eaf6277d88048403b2e5
Santiago-Romero <santiago.romero@correounivalle.edu.co>
2019-10-29 19:16:56 -0500

    cambios al modelo pizza y su despliegue en template

--- apps/pizzas/models.py
+++ apps/pizzas/models.py
@@ -4,5 +4,8 @@ class Pizza(models.Model):
     nombre = models.CharField(max_length=50)
     ingrediente = models.ManyToManyField('ingredientes.Ingrediente')
     valor = models.BigIntegerField()
+    descripcion = models.CharField(max_length=250,default="")
+    especial =  models.BooleanField(default=True)
+    enventa = models.BooleanField(default=True)
     def __str__(self):
         return self.nombre
