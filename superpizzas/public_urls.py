44bd76a61fc84125dc57940b33f9430c9fb13670
Stephany Rivera <rivera.stephany@correounivalle.edu.co>
2019-11-05 20:42:59 -0500

    actualizado login

--- superpizzas/public_urls.py
+++ superpizzas/public_urls.py
@@ -7,6 +7,7 @@ from apps.usuarios.views import inicio_sesion_admin, cerrar_sesion
 urlpatterns = [
     path('', inicio_franquicia, name='inicio'),
     path('compra/<str:tipo>', compra_franquicia, name='compra'),
+     path('accounts/', include('allauth.urls')),
     path('admin/', home_admin, name='home'),
     path('login/', inicio_sesion_admin,name='login'), 
     path('cerrar_sesion/',cerrar_sesion, name='cerrar_sesion'),    
