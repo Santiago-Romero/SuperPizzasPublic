bd2cbb2fd7afa3561fd60641214578fcf4e22768
JaqueToro <jaqueline.toro@correounivalle.edu.co>
2019-11-11 16:38:20 -0500

    Para mostrar logo
    Pendiente lograr guardar imagen automaticamente como se requiere

--- apps/franquicias/models.py
+++ apps/franquicias/models.py
@@ -4,18 +4,20 @@ from django_tenants.models import TenantMixin, DomainMixin
 ## from colorfield.fields import ColorField
 ## para el color pip install django-colorfield
 
+	 
+
 class TipoFranquicia(models.Model):
     nombre = models.CharField(max_length=100)  
     precio= models.IntegerField(default=0)
-
     def __str__(self):
         return self.nombre
-
+					
+		 
 class Franquicia(TenantMixin):
     nombre = models.CharField(max_length=50)    
     fecha_corte =  models.DateField(auto_now_add=True)
     tipo = models.ForeignKey(TipoFranquicia,default=None, on_delete=models.CASCADE)
-    imagen = models.ImageField(upload_to = 'images', default = 'images/logo.png')
+    imagen = models.ImageField(upload_to = 'images', default = 'logo.png')
     ##primario = ColorField(default='#FF0000')
     ##secundario = ColorField(default='#FF0000').
     working=models.BooleanField(default=True)
